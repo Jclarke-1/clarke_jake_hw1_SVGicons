043db430d31e22291bc18d69e190599a88e84505
Jclarke-1 <j_clarke226924@fanshaweonline.ca>
2026-01-29 13:20:45 -0500

    Added JS for Orangesvg
    Added Js to override the CSS for orangesvg and to allow it to change colours and log the ID when it is clicked on

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log("JS file connected");
+
+const orangesvg = document.querySelector("#Orange");
+console.log(orangesvg);
+
+function logID() {
+    console.log("User Clicked On:", this.id)
+}
+
+orangesvg.addEventListener("click", logID);
